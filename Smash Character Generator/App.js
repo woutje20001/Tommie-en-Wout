d38a22af66c8723bcf3799c7f3f9e581e38e03b1
Wout van Riel <PS183072@summacollege.nl>
2019-02-15 14:14:50 +0100

    Sound

--- Smash Character Generator/App.js
+++ Smash Character Generator/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image, Button, Alert, Icon, Linking, AsyncStora
 import { Grid, Row, Col } from 'react-native-easy-grid';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import { Audio } from 'expo';
 
 
 class HomeScreen extends React.Component {
@@ -13,462 +14,539 @@ class HomeScreen extends React.Component {
         id: 1,
         name: "Mario",
         requireurl: require("./img/Mario.png"),
+        audio: require("./sound/Mario.wav"),
         enabled: true
       },
       {
         id: 2,
         name: "DK",
         requireurl: require("./img/DK.png"),
+        audio: require("./sound/DK.wav"),
         enabled: true
       },
       {
         id: 3,
         name: "Link",
         requireurl: require("./img/Link.png"),
+        audio: require("./sound/Link.wav"),
         enabled: true
       },
       {
         id: 4,
         name: "Samus",
         requireurl: require("./img/Samus.png"),
+        audio: require("./sound/Samus.wav"),
         enabled: true
       },
       {
         id: 5,
         name: "Dark Samus",
         requireurl: require("./img/Dark_Samus.png"),
+        audio: require("./sound/Dark_Samus.wav"),
         enabled: true
       },
       {
         id: 6,
         name: "Yoshi",
         requireurl: require("./img/Yoshi.png"),
+        audio: require("./sound/Yoshi.wav"),
         enabled: true
       },
       {
         id: 7,
         name: "Kirby",
         requireurl: require("./img/Kirby.png"),
+        audio: require("./sound/Kirby.wav"),
         enabled: true
       },
       {
         id: 8,
         name: "Fox",
         requireurl: require("./img/Fox.png"),
+        audio: require("./sound/Fox.wav"),
         enabled: true
       },
       {
         id: 9,
         name: "Pikachu",
         requireurl: require("./img/Pikachu.png"),
+        audio: require("./sound/Pikachu.wav"),
         enabled: true
       },
       {
         id: 10,
         name: "Luigi",
         requireurl: require("./img/Luigi.png"),
+        audio: require("./sound/Luigi.wav"),
         enabled: true
       },
       {
         id: 11,
         name: "Ness",
         requireurl: require("./img/Ness.png"),
+        audio: require("./sound/Ness.wav"),
         enabled: true
       },
       {
         id: 12,
         name: "Captain Falcon",
         requireurl: require("./img/Captain_Falcon.png"),
+        audio: require("./sound/Captain_Falcon.wav"),
         enabled: true
       },
       {
         id: 13,
         name: "Jigglypuff",
         requireurl: require("./img/Jigglypuff.png"),
+        audio: require("./sound/Jigglypuff.wav"),
         enabled: true
       },
       {
         id: 14,
         name: "Peach",
         requireurl: require("./img/Peach.png"),
+        audio: require("./sound/Peach.wav"),
         enabled: true
       },
       {
         id: 15,
         name: "Daisy",
         requireurl: require("./img/Daisy.png"),
+        audio: require("./sound/Daisy.wav"),
         enabled: true
       },
       {
         id: 16,
         name: "Bowser",
         requireurl: require("./img/Bowser.png"),
+        audio: require("./sound/Bowser.wav"),
         enabled: true
       },
       {
         id: 17,
         name: "Ice Climbers",
         requireurl: require("./img/Ice_Climbers.png"),
+        audio: require("./sound/Ice_Climbers.wav"),
         enabled: true
       },
       {
         id: 18,
         name: "Sheik",
         requireurl: require("./img/Sheik.png"),
+        audio: require("./sound/Sheik.wav"),
         enabled: true
       },
       {
         id: 19,
         name: "Zelda",
         requireurl: require("./img/Zelda.png"),
+        audio: require("./sound/Zelda.wav"),
         enabled: true
       },
       {
         id: 20,
         name: "Dr. Mario",
         requireurl: require("./img/Dr_Mario.png"),
+        audio: require("./sound/Dr_Mario.wav"),
         enabled: true
       },
       {
         id: 21,
         name: "Pichu",
         requireurl: require("./img/Pichu.png"),
+        audio: require("./sound/Pichu.wav"),
         enabled: true
       },
       {
         id: 22,
         name: "Falco",
         requireurl: require("./img/Falco.png"),
+        audio: require("./sound/Falco.wav"),
         enabled: true
       },
       {
         id: 23,
         name: "Marth",
         requireurl: require("./img/Marth.png"),
+        audio: require("./sound/Marth.wav"),
         enabled: true
       },
       {
         id: 24,
         name: "Lucina",
         requireurl: require("./img/Lucina.png"),
+        audio: require("./sound/Lucina.wav"),
         enabled: true
       },
       {
         id: 25,
         name: "Young Link",
         requireurl: require("./img/Young_Link.png"),
+        audio: require("./sound/Young_Link.wav"),
         enabled: true
       },
       {
         id: 26,
         name: "Ganondorf",
         requireurl: require("./img/Ganondorf.png"),
+        audio: require("./sound/Ganondorf.wav"),
         enabled: true
       },
       {
         id: 27,
         name: "Mewtwo",
         requireurl: require("./img/Mewtwo.png"),
+        audio: require("./sound/Mewtwo.wav"),
         enabled: true
       },
       {
         id: 28,
         name: "Roy",
         requireurl: require("./img/Roy.png"),
+        audio: require("./sound/Roy.wav"),
         enabled: true
       },
       {
         id: 29,
         name: "Chrom",
         requireurl: require("./img/Chrom.png"),
+        audio: require("./sound/Chrom.wav"),
         enabled: true
       },
       {
         id: 30,
         name: "Mr. Game and Watch",
         requireurl: require("./img/Mr_Game_and_Watch.png"),
+        audio: require("./sound/Mr_Game_and_Watch.wav"),
         enabled: true
       },
       {
         id: 31,
         name: "Meta Knight",
         requireurl: require("./img/Meta_Knight.png"),
+        audio: require("./sound/Meta_Knight.wav"),
         enabled: true
       },
       {
         id: 32,
         name: "Pit",
         requireurl: require("./img/Pit.png"),
+        audio: require("./sound/Pit.wav"),
         enabled: true
       },
       {
         id: 33,
         name: "Dark Pit",
         requireurl: require("./img/Dark_Pit.png"),
+        audio: require("./sound/Dark_Pit.wav"),
         enabled: true
       },
       {
         id: 34,
         name: "Zero Suit Samus",
         requireurl: require("./img/Zero_Suit_Samus.png"),
+        audio: require("./sound/Zero_Suit_Samus.wav"),
         enabled: true
       },
       {
         id: 35,
         name: "Wario",
         requireurl: require("./img/Wario.png"),
+        audio: require("./sound/Wario.wav"),
         enabled: true
       },
       {
         id: 36,
         name: "Snake",
         requireurl: require("./img/Snake.png"),
+        audio: require("./sound/Snake.wav"),
         enabled: true
       },
       {
         id: 37,
         name: "Ike",
         requireurl: require("./img/Ike.png"),
+        audio: require("./sound/Ike.wav"),
         enabled: true
       },
       {
         id: 38,
         name: "Squirtle",
         requireurl: require("./img/Squirtle.png"),
+        audio: require("./sound/Pokemon_Trainer.wav"),
         enabled: true
       },
       {
         id: 39,
         name: "Ivysaur",
         requireurl: require("./img/Ivysaur.png"),
+        audio: require("./sound/Pokemon_Trainer.wav"),
         enabled: true
       },
       {
         id: 40,
         name: "Charizard",
         requireurl: require("./img/Charizard.png"),
+        audio: require("./sound/Pokemon_Trainer.wav"),
         enabled: true
       },
       {
         id: 41,
         name: "Diddy Kong",
         requireurl: require("./img/Diddy_Kong.png"),
+        audio: require("./sound/Diddy_Kong.wav"),
         enabled: true
       },
       {
         id: 42,
         name: "Lucas",
         requireurl: require("./img/Lucas.png"),
+        audio: require("./sound/Lucas.wav"),
         enabled: true
       },
       {
         id: 43,
         name: "Sonic",
         requireurl: require("./img/Sonic.png"),
+        audio: require("./sound/Sonic.wav"),
         enabled: true
       },
       {
         id: 44,
         name: "King Dedede",
         requireurl: require("./img/King_Dedede.png"),
+        audio: require("./sound/King_Dedede.wav"),
         enabled: true
       },
       {
         id: 45,
         name: "Olimar",
         requireurl: require("./img/Olimar.png"),
+        audio: require("./sound/Olimar.wav"),
         enabled: true
       },
       {
         id: 46,
         name: "Lucario",
         requireurl: require("./img/Lucario.png"),
+        audio: require("./sound/Lucario.wav"),
         enabled: true
       },
       {
         id: 47,
         name: "R.O.B.",
         requireurl: require("./img/ROB.png"),
+        audio: require("./sound/ROB.wav"),
         enabled: true
       },
       {
         id: 48,
         name: "Toon Link",
         requireurl: require("./img/Toon_Link.png"),
+        audio: require("./sound/Toon_Link.wav"),
         enabled: true
       },
       {
         id: 49,
         name: "Wolf",
         requireurl: require("./img/Wolf.png"),
+        audio: require("./sound/Wolf.wav"),
         enabled: true
       },
       {
         id: 50,
         name: "Villager",
         requireurl: require("./img/Villager.png"),
+        audio: require("./sound/Villager.wav"),
         enabled: true
       },
       {
         id: 51,
         name: "Mega Man",
         requireurl: require("./img/Mega_Man.png"),
+        audio: require("./sound/Mega_Man.wav"),
         enabled: true
       },
       {
         id: 52,
         name: "Wii Fit Trainer",
         requireurl: require("./img/Wii_Fit_Trainer.png"),
+        audio: require("./sound/Wii_Fit_Trainer.wav"),
         enabled: true
       },
       {
         id: 53,
         name: "Rosalina & Luma",
         requireurl: require("./img/Rosalina.png"),
+        audio: require("./sound/Rosalina.wav"),
         enabled: true
       },
       {
         id: 54,
         name: "Little Mac",
         requireurl: require("./img/Little_Mac.png"),
+        audio: require("./sound/Little_Mac.wav"),
         enabled: true
       },
       {
         id: 55,
         name: "Greninja",
         requireurl: require("./img/Greninja.png"),
+        audio: require("./sound/Greninja.wav"),
         enabled: true
       },
       {
         id: 56,
         name: "Mii Brawler",
         requireurl: require("./img/Mii_Brawler.png"),
+        audio: require("./sound/Mii_Brawler.wav"),
         enabled: true
       },
       {
         id: 57,
         name: "Mii Swordfighter",
         requireurl: require("./img/Mii_Swordfighter.png"),
+        audio: require("./sound/Mii_Swordfighter.wav"),
         enabled: true
       },
       {
         id: 58,
         name: "Mii Gunner",
         requireurl: require("./img/Mii_Gunner.png"),
+        audio: require("./sound/Mii_Gunner.wav"),
         enabled: true
       },
       {
         id: 59,
         name: "Palutena",
         requireurl: require("./img/Palutena.png"),
+        audio: require("./sound/Palutena.wav"),
         enabled: true
       },
       {
         id: 60,
         name: "Pac-Man",
         requireurl: require("./img/Pac-man.png"),
+        audio: require("./sound/Pac-Man.wav"),
         enabled: true
       },
       {
         id: 61,
         name: "Robin",
         requireurl: require("./img/Robin.png"),
+        audio: require("./sound/Robin.wav"),
         enabled: true
       },
       {
         id: 62,
         name: "Shulk",
         requireurl: require("./img/Shulk.png"),
+        audio: require("./sound/Shulk.wav"),
         enabled: true
       },
       {
         id: 63,
         name: "Bowser Jr.",
         requireurl: require("./img/Bowser_Jr.png"),
+        audio: require("./sound/Bowser_Jr.wav"),
         enabled: true
       },
       {
         id: 64,
         name: "Duck Hunt",
         requireurl: require("./img/Duck_Hunt.png"),
+        audio: require("./sound/Duck_Hunt.wav"),
         enabled: true
       },
       {
         id: 65,
         name: "Ryu",
         requireurl: require("./img/Ryu.png"),
+        audio: require("./sound/Ryu.wav"),
         enabled: true
       },
       {
         id: 66,
         name: "Ken",
         requireurl: require("./img/Ken.png"),
+        audio: require("./sound/Ken.wav"),
         enabled: true
       },
       {
         id: 67,
         name: "Cloud",
         requireurl: require("./img/Cloud.png"),
+        audio: require("./sound/Cloud.wav"),
         enabled: true
       },
       {
         id: 68,
         name: "Corrin",
         requireurl: require("./img/Corrin.png"),
+        audio: require("./sound/Corrin.wav"),
         enabled: true
       },
       {
         id: 69,
         name: "Bayonetta",
         requireurl: require("./img/Bayonetta.png"),
+        audio: require("./sound/Bayonetta.wav"),
         enabled: true
       },
       {
         id: 70,
         name: "Inkling",
         requireurl: require("./img/Inkling.png"),
+        audio: require("./sound/Inkling.wav"),
         enabled: true
       },
       {
         id: 71,
         name: "Ridley",
         requireurl: require("./img/Ridley.png"),
+        audio: require("./sound/Ridley.wav"),
         enabled: true
       },
       {
         id: 72,
         name: "Simon",
         requireurl: require("./img/Simon.png"),
+        audio: require("./sound/Simon.wav"),
         enabled: true
       },
       {
         id: 73,
         name: "Richter",
         requireurl: require("./img/Richter.png"),
+        audio: require("./sound/Richter.wav"),
         enabled: true
       },
       {
         id: 74,
         name: "King k Rool",
         requireurl: require("./img/King_K_Rool.png"),
+        audio: require("./sound/King_K_Rool.wav"),
         enabled: true
       },
       {
         id: 75,
         name: "Isabelle",
         requireurl: require("./img/Isabelle.png"),
+        audio: require("./sound/Isabelle.wav"),
         enabled: true
       },
       {
         id: 76,
         name: "Incineroar",
         requireurl: require("./img/Incineroar.png"),
+        audio: require("./sound/Incineroar.wav"),
         enabled: true
       },
       {
         id: 77,
         name: "Piranha Plant",
         requireurl: require("./img/Piranha_Plant.png"),
+        audio: require("./sound/Piranha_Plant.wav"),
         enabled: true
       },
       ],
@@ -482,7 +560,7 @@ class HomeScreen extends React.Component {
 
   _storeData = async () => {
     try {
-      
+
       await AsyncStorage.setItem('@ArrayChar:key', this.state.arraychar);
     } catch (error) {
       Alert.alert(error);
@@ -493,7 +571,7 @@ class HomeScreen extends React.Component {
       const value = await AsyncStorage.getItem('ArrayChar');
       if (value !== null) {
         // We have data!!
-        this.setState({arraychar: value})
+        this.setState({ arraychar: value })
         console.log(value);
       }
       else {
@@ -505,7 +583,11 @@ class HomeScreen extends React.Component {
   }
   /* ASYNC */
 
-  _onPressButton = (e) => {
+  test = () => {
+    Alert.alert("Loading");
+  }
+
+  _onPressButton = async (e) => {
     // Random generator
     const max = this.state.arraychar.length;
     const rand = Math.floor(Math.random() * max);
@@ -521,6 +603,16 @@ class HomeScreen extends React.Component {
 
     this.state.characterSource = value.requireurl;
     this.state.characterName = value.name;
+
+    Audio.setIsEnabledAsync(true)
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(value.audio);
+      await soundObject.playAsync();
+      // Your sound is playing!
+    } catch (error) {
+      // An error occurred!
+    }
   }
 
   setRandom = () => {
@@ -531,16 +623,8 @@ class HomeScreen extends React.Component {
       this.setState({ random: rand });
     }
   }
-  
+
   render() {
-    if (!this.state.isReady) {
-      return (
-        <AppLoading
-          startAsync={this._retrieveData}
-          onError={console.warn}
-        />
-      );
-    }
     return (
       <Grid onTouchStart={this.setRandom()}>
 
@@ -611,27 +695,31 @@ class AboutScreen extends React.Component {
       <Grid style={{
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'white'}}>
-        <Row style={{height: '4%'}}></Row>
+        backgroundColor: 'white'
+      }}>
+        <Row style={{ height: '4%' }}></Row>
         <Row style={{
           height: '10%',
           alignItems: 'center',
           justifyContent: 'center',
-          backgroundColor: 'white'}}>
+          backgroundColor: 'white'
+        }}>
           <Text>App made by Tommie Olislagers and Wout van Riel</Text>
         </Row>
         <Row style={{
           height: '15%',
           alignItems: 'center',
           justifyContent: 'center',
-          backgroundColor: 'white'}}>
+          backgroundColor: 'white'
+        }}>
           <Button color='tomato' onPress={this.privacyPolicy} title="View our Privacy Policy"></Button>
         </Row>
         <Row style={{
           height: '15%',
           alignItems: 'center',
           justifyContent: 'center',
-          backgroundColor: 'white'}}>
+          backgroundColor: 'white'
+        }}>
           <Button color='tomato' onPress={this.mailUs} title="Mail Us"></Button>
         </Row>
       </Grid>
